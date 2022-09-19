2e436744a4aff7889a6612a049ab9ad9de27ab70
Omar Morelos <>
2022-09-19 13:37:30 +0800

    Upgrade terraform and aws provider
    - Upgrade to aws provider 4.30.0
    - Some formatting from the vs code autoformat plugin.

--- main.tf
+++ main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.11.0"
+      version = "~> 4.30.0"
     }
   }
 
-  required_version = ">= 1.1.9"
+  required_version = ">= 1.2.7"
 }
 
 provider "aws" {
