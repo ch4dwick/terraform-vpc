1c4c74a55a34ead545092b4bb93d49b7b778f0fa
Omar Chad <ocmorelos@gmail.com>
2023-10-19 16:06:11 +0800

    Update main.tf
    - Add S3 Bucket backend config
    - Upgrade provider and client versions

--- main.tf
+++ main.tf
@@ -1,12 +1,18 @@
 terraform {
+  # Optional: If you want to use Amazon S3 to store your TF state.
+  #backend "s3" {
+  #  bucket = "my-bucket"
+  #  key    = "vpc-state"
+  #  region = "ap-southeast-1"
+  # }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.58.0"
+      version = "~> 5.21.0"
     }
   }
 
-  required_version = ">= 1.4.0"
+  required_version = ">= 1.6.2"
 }
 
 provider "aws" {
