4c4792ccdf74610bea1eaf530e8bc17e81252fcb
ch4dwick <omar@xld.finance>
2024-01-06 00:18:32 +0800

    upgrade tf provider and client

--- main.tf
+++ main.tf
@@ -8,11 +8,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.21.0"
+      version = "~> 5.31.0"
     }
   }
 
-  required_version = ">= 1.6.2"
+  required_version = ">= 1.6.6"
 }
 
 provider "aws" {
